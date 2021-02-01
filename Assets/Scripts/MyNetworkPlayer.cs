696bad820c4bd132d5d60fe2d77fe1d71a771c76
warmoverdrive <sweetsthebear@gmail.com>
2021-02-01 11:37:18 -0800

    Set up hook methods for SyncVars to update variables across clients on server

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-	[SyncVar]
+	[SerializeField] private TMP_Text displayNameText = null;
+	[SerializeField] private Renderer displayColorRenderer = null;
+
+	[SyncVar(hook = nameof(HandleDisplayNameUpdated))]
 	[SerializeField]
 	private string displayName = "Missing Name";
-	[SyncVar]
+
+	[SyncVar(hook = nameof(HandleDisplayColorUpdated))]
 	[SerializeField]
 	private Color displayColor = Color.white;
 
@@ -18,4 +23,14 @@ public class MyNetworkPlayer : NetworkBehaviour
 	public string GetDisplayName() { return displayName; }
 	[Server]
 	public void SetDisplayColor(Color newColor) { displayColor = newColor; }
+
+	private void HandleDisplayColorUpdated(Color oldColor, Color newColor)
+	{
+		displayColorRenderer.material.SetColor("_BaseColor", newColor);
+	}
+
+	private void HandleDisplayNameUpdated(string oldName, string newName)
+	{
+		displayNameText.text = newName;
+	}
 }
